24c70732768f44c4ee40d41ed396931655eeb22b
Сергей <sergeystarod@mail.ru>
2022-09-29 21:27:49 +0300

    Мой первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        //Задание 2
+        System.out.println(fullName.toUpperCase());
+
+        //Задание 3
+        String fullName_2 = "Иванов Семён Семёнович";
+        String fullName_3 = fullName_2.replace("ё", "е");
+        System.out.println(fullName_3);
     }
 }
